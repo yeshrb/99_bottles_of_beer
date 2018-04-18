db3edec7478b69776c552cb7c19889128d8a8d69
ljj <yeshrb@126.com>
2018-04-18 21:58:39 +0800

    complete

--- spec/99_bottles_of_beer_spec.js
+++ spec/99_bottles_of_beer_spec.js
@@ -3,14 +3,12 @@ describe('lyric',() =>{
     it("should be able to print 99_bottles_beer lyric", function() {
         const readline = require('readline');
         const fs = require('fs');
-        let expectText = '\n';
-
         const rl = readline.createInterface({
-            input: fs.createReadStream('/Users/ljj/proLearn/tw3/99_bottles_of_beer/spec/testData'),
+            input: fs.createReadStream('/Users/ljj/proLearn/tw3/99_bottles_of_beer/lyrics.md'),
             crlfDelay: Infinity
         });
 
-
+        let expectText = '';
         rl.on('line', (line) => {
             expectText += `${line.trim()}\n`;
 
